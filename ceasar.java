f7518e6ea0afcc8200a6fe19bf1639872ed49c6e
Aris John <arisercubickchannel@gmail.com>
2025-12-09 14:55:16 -0500

    Ceaser cipher final

--- ceasar.java
+++ ceasar.java
@@ -1,19 +1,28 @@
+import java.util.Scanner;
 public class ceasar { 
 	public static void main(String[] args) {
-		String as = "ab2Gced";
+		Scanner reader = new Scanner(System.in);
+		System.out.print("Give a word to decipher: ");
+		String as = reader.nextLine();
+		System.out.println();
+		System.out.println();
 		System.out.println(ceasar(as));
 	}
 	
 	public static String ceasar(String word) {
 		String newWord = "";
 		for (int i = 0; i < word.length(); i++) {
-			char let = word.charAt(i);
-			if (let >= 'a' && let <= 'z') {
-				newWord = newWord + small(let);
-			} else if (let >= 'A' && let <= 'Z') {
-				newWord = newWord + large(let);
+			if ((word.charAt(i) >= 'a' && word.charAt(i) <= 'z') || (word.charAt(i) >= 'A' && word.charAt(i) <= 'Z') || (word.charAt(i) >= '0' && word.charAt(i) <= '9')) {
+				char let = word.charAt(i);
+				if (let >= 'a' && let <= 'z') {
+					newWord = newWord + small(let);
+				} else if (let >= 'A' && let <= 'Z') {
+					newWord = newWord + large(let);
+				} else {
+					newWord = newWord + num(let);
+				}
 			} else {
-				newWord = newWord + num(let);
+				newWord = newWord + word.charAt(i);
 			}
 		}
 		return newWord;
@@ -21,22 +30,22 @@ public class ceasar {
 	
 	public static char small(char letter) {
 		if (letter > 'w') {
-			letter = (char) (letter - 'w');
+			return (char) (letter - ('x' - 'a'));
 		}
 		return (char)(letter + ('d' - 'a'));
 	}
 	
 	public static char large(char letter) {
 		if (letter > 'W') {
-			letter = (char) (letter - 'W');
+			return (char) (letter - ('X' - 'A'));
 		}
 		return (char)(letter + ('D' - 'A'));
 	}
 	
 	public static char num(char num) {
 		if (num > '6') {
-			num = (char) (num - '6');
+			return (char) (num - ('7' - '0'));
 		}
-		return (char)(num + ('4'-'1'));
+		return (char)(num + ('3'-'0'));
 	}
 }
