3f9405c7861c06ee75a84b236725e2c6399ef231
Aris John Apolinario <aris.apolinario@dawsoncollege.qc.ca>
2025-12-09 11:51:27 -0500

    pusged the info

--- mergeSort.java
+++ mergeSort.java
@@ -1,7 +1,7 @@
 public class mergeSort {
 	public static void main(String[] args) {
 		int[] arr = {4, 5, 9, 11, 15};
-		int[] arr2 = {6, 5, 4};
+		int[] arr2 = {4, 6, 5};
 		print(merge(arr, arr2));
 	}
 	
@@ -9,14 +9,21 @@ public class mergeSort {
 		int[] arr = new int[arr1.length + arr2.length];
 		
 		for (int i = 0, j = 0, k = 0; i < arr.length; i++) {
-			if (j < arr1.length && arr1[j] < arr2[k]) {
+			if (j != arr1.length && k != arr2.length) {
+				if (arr1[j] < arr2[k]) {
+					arr[i] = arr1[j];
+					j++;
+				}
+				else {
+					arr[i] = arr2[k];
+					k++; 
+				}
+			} else if (j != arr1.length) {
 				arr[i] = arr1[j];
 				j++;
-			}
-			if (k < arr2.length && arr1[j] > arr[k]) {
+			} else if (k != arr2.length) {
 				arr[i] = arr2[k];
-				if (k == arr2.length) {
-				k++;
+				k++; 
 			}
 		}
 		
