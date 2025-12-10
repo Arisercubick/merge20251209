508dbc73c4c8beae6a73c514ca766015ecb993a7
Aris John <arisercubickchannel@gmail.com>
2025-12-09 23:36:42 -0500

    final changes

--- ceasar.java
+++ ceasar.java
@@ -2,11 +2,18 @@ import java.util.Scanner;
 public class ceasar { 
 	public static void main(String[] args) {
 		Scanner reader = new Scanner(System.in);
-		System.out.print("Give a word to decipher: ");
-		String as = reader.nextLine();
-		System.out.println();
-		System.out.println();
-		System.out.println(ceasar(as));
+		boolean continueOn = true;
+		while (continueOn) {
+			System.out.print("Give a word to decipher (Type yes to end the loop): ");
+			String as = reader.nextLine();
+			if (as.toUpperCase().equals("YES")) {
+				continueOn = false;
+			} else {
+				System.out.println();
+				System.out.println();
+				System.out.println(ceasar(as));
+			}
+		}
 	}
 	
 	public static String ceasar(String word) {
